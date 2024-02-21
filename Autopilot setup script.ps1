d3467ee83b6d73bfc8fdb63bd2c566d36af6ef82
Adam Dilger <adamdilger@virginmedia.com>
2024-02-21 10:13:08 +0000

    Add files via upload

---
+++ Autopilot setup script.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy bypass
+Install-Module -Name PSWindowsUpdate -RequiredVersion 2.2.0.3
+Get-WindowsUpdate -Install -Verbose
+Install-Script Get-WindowsAutoPilotInfo
+Get-WindowsAutoPilotInfo.ps1 – Online 
